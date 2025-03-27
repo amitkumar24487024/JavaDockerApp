a9d550a82a947861ef21ad8925f8b3d1f08783d2
Amit Kumar Singh <61904992+amitkumar24487024@users.noreply.github.com>
2025-03-27 16:35:32 +0000

    Add REST controller with hello endpoint and configure server port

--- javaapp/src/main/java/com/example/amit/javaapp/JavaappApplication.java
+++ javaapp/src/main/java/com/example/amit/javaapp/JavaappApplication.java
@@ -2,12 +2,19 @@ package com.example.amit.javaapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController("/api")
 public class JavaappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaappApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(){
+		return "Hello World";
+	}
 }
